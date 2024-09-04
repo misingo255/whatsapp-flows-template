304ac4fe189421bfc266bfd28c3ce76ebfe4b7bc
Wilbert Misingo <wilbertmisingo@gmail.com>
2024-09-04 16:22:37 +0300

    fix

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -10,8 +10,6 @@ load_dotenv()
 
 WHATSAPP_BUSINESS_VERIFY_TOKEN = os.getenv("WHATSAPP_BUSINESS_VERIFY_TOKEN", "")
 
-if not WHATSAPP_BUSINESS_VERIFY_TOKEN:
-    raise ValueError("Please provide all required environment variables")
 
 webhook_router = APIRouter()
 
